362c41092f41baa9f6e7ac63fb5b2cb0b847170c
Sebastian Morales <51178433+SebaMorales74@users.noreply.github.com>
2025-09-25 12:50:51 -0300

    feat: mi primera clase en python
    👽👽👽👽
    Clase del 25 del 09
    ☆*: .｡. o(≧▽≦)o .｡.:*☆

---
+++ clase01u2.py
@@ -0,0 +1,18 @@
+class Cliente:
+    def __init__(self, nombre: str, rut: str, edad: int):
+        self.__nombre: str = nombre
+        self.__rut: str = rut
+        self__edad: int = edad
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+
+cliente1: Cliente = Cliente(
+    nombre="Felipe Villaroel",
+    rut="21789567-K",
+    edad=21
+)
+
+print(cliente1.nombre)
