1bf03dea2cd8c3353e27dcaf5a3243a10cd00056
Sebastian Morales <51178433+SebaMorales74@users.noreply.github.com>
2025-11-14 12:46:38 -0300

    14-11-2025

--- oracle_ejemplo/oracle_crud.py
+++ oracle_ejemplo/oracle_crud.py
@@ -1,18 +1,14 @@
-# Importamos librerias
 import oracledb
 import os
 from dotenv import load_dotenv
-# Cargamos variables de entorno
 load_dotenv()
-# Definimos valores gracias a las variables de entorno
 username = os.getenv("ORACLE_USER")
 dsn = os.getenv("ORACLE_DSN")
 password = os.getenv("ORACLE_PASSWORD")
-# Creamos una conexion reutilizable
+
 def get_connection():
     return oracledb.connect(user=username, password=password, dsn=dsn)
 
-# Funcion para crear el esquema de la base de datos
 def create_schema(query):
     try:
         with get_connection() as conn:
@@ -57,54 +53,59 @@ for query in tables:
     create_schema(query)
 
 
+# PERSONAS
+# Create - Inserción de datos
+from datetime import datetime
+def create_persona(
+        id: int,
+        rut: str,
+        nombres: str,
+        apellidos: str,
+        fecha_nacimiento: str
+):
+    sql = (
+        "INSERT INTO PERSONAS (id, rut, nombres, apellidos, fecha_nacimiento)"
+        "VALUES (:id,:rut,:nombres,:apellidos,:fecha_nacimiento)"
+    )
+
+    parametros = {
+        "id": id,
+        "rut": rut,
+        "nombres": nombres,
+        "apellidos": apellidos,
+        "fecha_nacimiento": datetime.strptime(fecha_nacimiento,"%Y-%m-%d")
+    }
+
+    try:
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql,parametros)
+                print(f"Dato insertado \n {parametros}")
+            conn.commit()
+    except oracledb.DatabaseError as e:
+        err = e
+        print(f"Error al insertar datos: {err} \n {parametros}")
+
+create_persona(
+    id=1,
+    rut="19456321",
+    nombres="Alejandra Maria",
+    apellidos="Mayorga Cayuqueo",
+    fecha_nacimiento="1998-08-30"
+)
 
 
+def create_departamento(
+        id: int,
+        nombre: str,
+        fecha_creacion: str
+):
+    pass
 
-
-# from datetime import datetime
-# # CREATE
-# def create_persona(rut, nombres, apellidos=None, fecha_nacimiento=None, cod_area="+569", numero_telefono=None):
-#     sql = (
-#     "INSERT INTO personas (rut, nombres, apellidos, fecha_nacimiento, cod_area, numero_telefono) "
-#     "VALUES (:rut, :nombres, :apellidos, :fecha_nacimiento, :cod_area, :numero_telefono)"
-#     )
-#     if fecha_nacimiento:
-#         bind_fecha = datetime.strptime(fecha_nacimiento, "%Y-%m-%d")
-#     else:
-#         bind_fecha = None
-#     with get_connection() as conn:
-#         with conn.cursor() as cur:
-#             cur.execute(sql, {
-#             "rut": rut,
-#             "nombres": nombres,
-#             "apellidos": apellidos,
-#             "fecha_nacimiento": bind_fecha,
-#             "cod_area": cod_area,
-#             "numero_telefono": numero_telefono,
-#             })
-#         conn.commit()
-#     print(f"Persona con RUT={rut} creada.")
-# create_persona(
-#     rut="12314984-9",
-#     nombres="Roberto Mandril",
-#     apellidos="Manda Jara"
-# )
-# # READ
-# def list_personas(limit: int = 100):
-#     sql = f"SELECT * FROM personas FETCH FIRST {limit} ROWS ONLY"
-#     results = []
-#     with get_connection() as conn:
-#         with conn.cursor() as cur:
-#             cur.execute(sql)
-#             for row in cur:
-#                 rut, nombres, apellidos, fecha_nacimiento, cod_area, numero_telefono = row
-#                 results.append({
-#                 "rut": rut,
-#                 "nombres": nombres,
-#                 "apellidos": apellidos,
-#                 "fecha_nacimiento": fecha_nacimiento.isoformat() if fecha_nacimiento else None,
-#                 "cod_area": cod_area,
-#                 "numero_telefono": numero_telefono,
-#                 })
-#     return results
-# print(list_personas())
+def create_empleado(
+        id: int,
+        sueldo: int,
+        idPersona: int,
+        idDepartamento: int
+):
+    pass
